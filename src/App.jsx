5ee637def5afb4ba63bd2961be940a3037bdf206
Rafly Romeo <94301619+raflyr23@users.noreply.github.com>
2025-01-19 20:31:38 +0000

    javascript-ms-project

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Rafly Romeo",
+  title: "Web & Dev Ops Engineer",
+  email: "raflyromeo3@gmail.com",
+  gitHub: "raflyr23",
+  instagram: "raflyromeoo",
+  linkedIn: "raflyromeo",
+  medium: "raflyromeo",
+  twitter: "rafly_23r",
+  youTube: "raflyromeo3120",
 };
 
 const primaryColor = "#4E567E";
